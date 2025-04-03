f9c0d211e0596c766aa36e73569b2b8f20634f3f
TaYaKi71751 <shantian.tw@gmail.com>
2025-04-03 10:00:47 +0900

    Fix

--- src/main/java/com/mlb/customer/ProfileServlet.java
+++ src/main/java/com/mlb/customer/ProfileServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class ProfileServlet
  */
-@WebServlet("customer/ProfileServlet")
+@WebServlet("/customer/ProfileServlet")
 public class ProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
